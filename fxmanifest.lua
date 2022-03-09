6ac3621f42760e0b6b059e95f0a6211ac20c25e7
Sahbes <80922759+Sahbes@users.noreply.github.com>
2022-03-09 13:50:14 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'Sahbes'
+description 'Usefull script for saving components for property, trunk, inventories etc'
+
+server_scripts {
+	'server.lua',
+}
+
